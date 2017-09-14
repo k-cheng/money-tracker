42d1cb55d780353757d621553326386e32c06b65
Kevin Cheng <09chengk@gmail.com>
2017-09-14 01:59:09 -0700

    can use Calendar functions

--- src/pages/search/search.ts
+++ src/pages/search/search.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { Calendar } from '@ionic-native/calendar';
 
 
 import { Item } from '../../models/item';
@@ -8,16 +9,22 @@ import { Items } from '../../providers/providers';
 @IonicPage()
 @Component({
   selector: 'page-search',
+  providers: [Calendar],
   templateUrl: 'search.html'
 })
 export class SearchPage {
 
   currentItems: any = [];
 
-  timeFrame: string;
-  budget: number;
+  public timeFrame: string;
+  public budget: number;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams, public items: Items) { }
+  constructor(
+    public navCtrl: NavController, 
+    public navParams: NavParams, 
+    public items: Items,
+    private calendar: Calendar
+  ) { }
 
   /**
    * Perform a service for the proper items.
@@ -40,6 +47,20 @@ export class SearchPage {
     this.navCtrl.push('ItemDetailPage', {
       item: item
     });
+  }
+
+  /**
+   * Navigate to the detail page for this item.
+   */
+  public createEvent(): void {
+    console.log('budget: ', this.budget)
+    this.calendar.createCalendar('MyCalendar')
+      .then((msg) => {
+         console.log(msg); 
+        },
+        (err) => {
+          console.log(err);
+      });
   }
 
 }
